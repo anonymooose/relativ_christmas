7778c47e74028b32112d05745bf84610b117b76a
Carla Almazora <carla.almazora@gmail.com>
2018-11-09 12:00:20 +0900

    add javascript video not fixed and style

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-
+$(function()
+  {
+    var video = document.createElement('video');
+    video.setAttribute('class','videoPlayer');
+    video.setAttribute('muted', '');
+    video.setAttribute('autoplay', '');
+    video.setAttribute('playsinline', '');
+});
