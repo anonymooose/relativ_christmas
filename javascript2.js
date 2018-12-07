0931533c62e95e11294a223a3348c1f4cc758723
Carla Almazora <carla.almazora@gmail.com>
2018-12-07 15:28:03 +0900

    fix secondary and style css, create new javascript file

---
+++ javascript2.js
@@ -0,0 +1,29 @@
+$(function()
+  {
+    var img = document.createElement('img');
+    img.setAttribute('src', 'relativ_holidaycard_2018_v1-01.png')
+
+    var filter = document.createElement('div');
+    document.body.appendChild(img);
+    document.body.appendChild(filter);
+
+    $(filter).addClass('filter');
+    $(filter).addClass('yellow');
+
+    filter.onclick = function()
+      {
+        if($(filter).hasClass("yellow")){
+          $(filter).removeClass("yellow");
+          $(filter).addClass("grey");
+        }else if($(filter).hasClass("grey")){
+          $(filter).removeClass("grey");
+          $(filter).addClass("orange");
+        }else if($(filter).hasClass("orange")){
+          $(filter).removeClass("orange");
+          $(filter).addClass("purple");
+        } else{
+          $(filter).removeClass("purple");
+          $(filter).addClass("yellow");
+        }
+      }
+});
